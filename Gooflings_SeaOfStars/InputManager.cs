37f6ce9f8476ed5a8409a0431c9c1bdfc99c3521
greboul <greboul@GCFRLY230>
2024-03-19 15:59:00 +0100

    Debut fin ce soir et on jouie

---
+++ Gooflings_SeaOfStars/InputManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gooflings
+{
+    public class InputManager
+    {
+
+        private Dictionary<string, bool> keyList;
+        public InputManager()
+        {
+            while (true)
+            {
+                ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
+                Console.WriteLine(consoleKeyInfo.Key);
+            }
+
+            /*keyList.Add("Escape", false);
+            keyList.Add("Enter", false);
+
+            keyList.Add("Up", false);
+            keyList.Add("Down", false);
+            keyList.Add("Left", false);
+            keyList.Add("Right", false);
+
+            keyList.Add("ArrowUp", false);
+            keyList.Add("ArrowDown", false);
+            keyList.Add("ArrowLeft", false);
+            keyList.Add("ArrowRight", false);*/
+
+        }
+
+        public bool getKeyDown(string key)
+        {
+            return keyList.GetValueOrDefault(key);
+        }      
+
+    }
+
+}
--- Gooflings_SeaOfStars/Program.cs
+++ Gooflings_SeaOfStars/Program.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using Gooflings;
+using System;
+
 
 namespace Gooflings
 {
+
     public class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("AHHH");
+
+            InputManager input = new InputManager();
+
         }
     }
 }
