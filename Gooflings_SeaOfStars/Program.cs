f1ec091a633f38f7361d5b8e55ad6a3447628087
William <wbailleul@gaming.tech>
2024-03-19 17:34:48 +0100

    Menu base

--- Gooflings_SeaOfStars/Program.cs
+++ Gooflings_SeaOfStars/Program.cs
@@ -7,6 +7,11 @@ namespace Gooflings
         public static void Main(string[] args)
         {
             Resources resources = new Resources();
+            Menu menu = new Menu();
+
+            menu.DrawMainMenu();
+            menu.DrawTeamMenu();
+            menu.DrawBagMenu();
         }
     }
 }
