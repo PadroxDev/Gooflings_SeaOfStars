33933ab667e78dbfc03d1e472b50f40f9a218f89
Padrox <antoinevollet.pro@gmail.com>
2024-03-24 17:20:23 +0100

    j'ai la flemme

--- Gooflings_SeaOfStars/Program.cs
+++ Gooflings_SeaOfStars/Program.cs
@@ -3,62 +3,75 @@ using Gooflings.Moves;
 
 namespace Gooflings
 {
-
     public class Program
     {
         public static void Main(string[] args)
         {
-
+            // Encapsuler dans une class App
             Console.Title = "Gooflings";
             Console.SetWindowSize(180,50);
-          
+
+            // Initialization
+            Resources resources = new Resources();
             MovementPlayer movement = new MovementPlayer();
             Player player = new Player();
-            InputManager input = new InputManager();
+            InputManager inputManager = new InputManager();
+            NavMenu nav = new(inputManager, 3, 4);
 
-            //while (true)
-            //{
-            //    bool pressed = Console.KeyAvailable;
-            //    movement.DoesMove(pressed, player, input);
+            // Gameloop
+            while (true) {
+                inputManager.Update();
+                nav.Update();
 
-            //    for (int i = 0; i < 10; i++)
-            //    {
-            //        for (int j = 0;j < 10; j++)
-            //        {
-            //            if (i == player.posY && j == player.posX)
-            //            {
-            //                Console.Write("0");
-            //            }
-            //            else
-            //            {
-            //                Console.Write(".");
-            //            }
-                        
-            //        } 
-            //        Console.WriteLine();
-            //    }
+                int[,] buffer = new int[nav.Columns, nav.Rows];
 
-            //    Thread.Sleep(1000);
-            //    Console.Clear();
+                for (int x = 0; x < nav.Columns; x++) {
+                    for (int y = 0; y < nav.Rows; y++) {
+                        int v = x * nav.Rows + y;
+                        buffer[x, y] = nav.SelectedIndex == v ? 'S' : v;
+                    }
+                }
 
-            //}
+                for (int x = 0;  x < nav.Rows;  x++) {
+                    for (int y = 0; y < nav.Columns; y++) {
+                        Console.Write(buffer[y, x] == 83 ? "P ," : buffer[y, x] + " ,");
+                    }
+                    Console.WriteLine();
+                }
+
+                //movement.DoesMove(player, inputManager);
+
+                //for (int i = 0; i < 10; i++) {
+                //    for (int j = 0; j < 10; j++) {
+                //        if (i == player.posY && j == player.posX) {
+                //            Console.Write("0");
+                //        } else {
+                //            Console.Write(".");
+                //        }
+                //    }
+                //    Console.WriteLine();
+                //}
+
+                Thread.Sleep(250);
+                Console.Clear();
+            }
 
 
-            Resources resources = new Resources();
-            Menu menu = new Menu();
-            GooflingData grayanData = resources.GetGooflingData(GooflingType.Grayan);
-            grayanData.Level = 20;
-            Goofling grayan = new(grayanData);
-            GooflingData danyData = resources.GetGooflingData(GooflingType.Radany);
-            danyData.CurrentHP = 0.8252427f;
 
-            danyData.Level = 32;
-            Goofling dany = new(danyData);
+            //Menu menu = new Menu();
+            //GooflingData grayanData = resources.GetGooflingData(GooflingType.Grayan);
+            //grayanData.Level = 20;
+            //Goofling grayan = new(grayanData);
+            //GooflingData danyData = resources.GetGooflingData(GooflingType.Radany);
+            //danyData.CurrentHP = 0.8252427f;
 
-            player.Party.Members.Add(grayan);
-            player.Party.Members.Add(dany);
+            //danyData.Level = 32;
+            //Goofling dany = new(danyData);
 
-            Console.WriteLine(dany);
+            //player.Party.Members.Add(grayan);
+            //player.Party.Members.Add(dany);
+
+            //Console.WriteLine(dany);
             //Move move = resources.GetMove(MoveType.Croustifesses);
             //move.OnAction(grayan, dany);
         }
