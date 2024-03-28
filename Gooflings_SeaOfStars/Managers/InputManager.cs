64ad279c3084a1e1c42db841060187cca4be4257
William <wbailleul@gaming.tech>
2024-03-28 18:09:30 +0100

    fix input post merge

--- Gooflings_SeaOfStars/Managers/InputManager.cs
+++ Gooflings_SeaOfStars/Managers/InputManager.cs
@@ -3,27 +3,34 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 
-namespace Gooflings {
-    public enum KeyState {
+namespace Gooflings
+{
+    public enum KeyState
+    {
         Up,
         Pressed,
         Down,
         Released
     }
 
-    public class InputManager {
+    public class InputManager
+    {
         private Dictionary<ConsoleKey, KeyState> _keyDict;
-            keyList = new Dictionary<string, bool>();
-        public InputManager() {
+
+        public InputManager()
+        {
             _keyDict = new();
 
-            foreach (ConsoleKey key in Enum.GetValues(typeof(ConsoleKey))) {
+            foreach (ConsoleKey key in Enum.GetValues(typeof(ConsoleKey)))
+            {
                 _keyDict[key] = KeyState.Up;
             }
         }
 
-        void UpdatePressedKey(ConsoleKey key) {
-            switch (_keyDict[key]) {
+        void UpdatePressedKey(ConsoleKey key)
+        {
+            switch (_keyDict[key])
+            {
                 case KeyState.Up:
                 case KeyState.Released:
                     _keyDict[key] = KeyState.Pressed;
@@ -35,9 +42,11 @@ namespace Gooflings {
                     break;
             }
         }
-        }
-        private void UpdateRemainingKey(ConsoleKey key) {
-            switch (_keyDict[key]) {
+
+        private void UpdateRemainingKey(ConsoleKey key)
+        {
+            switch (_keyDict[key])
+            {
                 case KeyState.Pressed:
                 case KeyState.Down:
                     _keyDict[key] = KeyState.Released;
@@ -50,24 +59,25 @@ namespace Gooflings {
             }
         }
 
-        public void Update() {
+        public void Update()
+        {
             List<ConsoleKey> keysToHandle = _keyDict.Where(p => p.Value != KeyState.Up)
                 .ToDictionary(p => p.Key, p => p.Value).Keys.ToList();
-                // comment faire pour que ce s'active que quand la touche est lacher 
-            while (Console.KeyAvailable) {
+
+            while (Console.KeyAvailable)
+            {
                 ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-                keyList = keyList.ToDictionary(p => p.Key, p => false);
-                keyList = keyList.ToDictionary(p => p.Key, p => false);
 
                 if (keysToHandle.Contains(keyInfo.Key))
                     keysToHandle.Remove(keyInfo.Key);
 
                 UpdatePressedKey(keyInfo.Key);
-            foreach (var key in keysToHandle) {
-                UpdateRemainingKey(key);
-            }
             }
 
+            foreach (var key in keysToHandle)
+            {
+                UpdateRemainingKey(key);
+            }
         }
 
         public bool GetKeyDown(ConsoleKey key) => _keyDict[key] == KeyState.Pressed;
